700983a69b1e55ee66af4fb58ff82308fc8dfa1f
Dimitri Nazari <dima_nazari@hotmail.com>
2015-11-04 11:21:39 +0100

    ayy

--- barroc_IT/barroc_IT/DataDict.cs
+++ barroc_IT/barroc_IT/DataDict.cs
@@ -6,7 +6,181 @@ using System.Threading.Tasks;
 
 namespace barroc_IT
 {
-    class DataDict
+    public class DataDict
     {
+        #region 10customers
+        /// <summary>
+        /// tbl_customers
+        /// </summary>
+        public const string TBL10 = "tbl_customers";
+        /// <summary>
+        /// customer_ID
+        /// </summary>
+        public const string FLD11 = "customer_ID";
+        /// <summary>
+        /// company_Name
+        /// </summary>
+        public const string FLD12 = "company_Name";
+        /// <summary>
+        /// address1
+        /// </summary>
+        public const string FLD13 = "address1";
+        /// <summary>
+        /// zipcode1
+        /// </summary>
+        public const string FLD14 = "zipcode1";
+        /// <summary>
+        /// city1
+        /// </summary>
+        public const string FLD15 = "city1";
+        /// <summary>
+        /// address2
+        /// </summary>
+        public const string FLD16 = "address2";
+        /// <summary>
+        /// zipcode2
+        /// </summary>
+        public const string FLD17 = "zipcode2";
+        /// <summary>
+        /// city2
+        /// </summary>
+        public const string FLD18 = "city2";
+        /// <summary>
+        /// contact_Person
+        /// </summary>
+        public const string FLD19 = "contact_Person";
+        /// <summary>
+        /// initials
+        /// </summary>
+        public const string FLD110 = "initials";
+        /// <summary>
+        /// phone_Num1
+        /// </summary>
+        public const string FLD111 = "phone_Num1";
+        /// <summary>
+        /// phone_Num2
+        /// </summary>
+        public const string FLD112 = "phone_Num2";
+        #endregion
+        #region 20projects
+        /// <summary>
+        /// tbl_projects
+        /// </summary>
+        public const string TBL20 = "tbl_projects";
+        /// <summary>
+        /// project_ID
+        /// </summary>
+        public const string FLD21 = "project_ID";
+        /// <summary>
+        /// app_Name
+        /// </summary>
+        public const string FLD22 = "app_Name";
+        /// <summary>
+        /// maintenance_Contract
+        /// </summary>
+        public const string FLD23 = "maintenance_Contract";
+        /// <summary>
+        /// credit_Worthy
+        /// </summary>
+        public const string FLD24 = "credit_Worthy";
+        /// <summary>
+        /// bkr
+        /// </summary>
+        public const string FLD25 = "bkr";
+        /// <summary>
+        /// price
+        /// </summary>
+        public const string FLD26 = "price";
+        /// <summary>
+        /// limit
+        /// </summary>
+        public const string FLD27 = "limit";
+        /// <summary>
+        /// amount_of_Invoices
+        /// </summary>
+        public const string FLD28 = "amount_of_Invoices";
+        #endregion
+        #region 30appointments
+        /// <summary>
+        /// tbl_appointments
+        /// </summary>
+        public const string TBL30 = "tbl_appointments";
+        /// <summary>
+        /// appointment_ID
+        /// </summary>
+        public const string FLD31 = "appointment_ID";
+        /// <summary>
+        /// subject
+        /// </summary>
+        public const string FLD32 = "subject";
+        /// <summary>
+        /// date
+        /// </summary>
+        public const string FLD33 = "date";
+        /// <summary>
+        /// location
+        /// </summary>
+        public const string FLD34 = "location";
+        /// <summary>
+        /// time
+        /// </summary>
+        public const string FLD35 = "time";
+        #endregion
+        #region 40invoices
+        /// <summary>
+        /// tbl_invoices
+        /// </summary>
+        public const string TBL40 = "tbl_invoices";
+        /// <summary>
+        /// invoice_ID
+        /// </summary>
+        public const string FLD41 = "invoice_ID";
+        /// <summary>
+        /// status
+        /// </summary>
+        public const string FLD42 = "status";
+        /// <summary>
+        /// debit_Num
+        /// </summary>
+        public const string FLD43 = "debit_Num";
+        /// <summary>
+        /// credit_Num
+        /// </summary>
+        public const string FLD44 = "credit_Num";
+        /// <summary>
+        /// paid
+        /// </summary>
+        public const string FLD45 = "paid";
+        /// <summary>
+        /// date
+        /// </summary>
+        public const string FLD46 = "date";
+        /// <summary>
+        /// invoice_price
+        /// </summary>
+        public const string FLD47 = "invoice_price";
+        #endregion
+        #region 50users
+        /// <summary>
+        /// tbl_users
+        /// </summary>
+        public const string TBL50 = "tbl_users";
+        /// <summary>
+        /// user_ID
+        /// </summary>
+        public const string FLD51 = "user_ID";
+        /// <summary>
+        /// username
+        /// </summary>
+        public const string FLD52 = "username";
+        /// <summary>
+        /// password
+        /// </summary>
+        public const string FLD53 = "password";
+        /// <summary>
+        /// department
+        /// </summary>
+        public const string FLD54 = "department";
+        #endregion
     }
 }
